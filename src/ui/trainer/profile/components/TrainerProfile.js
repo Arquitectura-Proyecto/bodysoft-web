1727cce6841a9c299caa42df07d661aa61f52828
jestupinanb <30636279+jestupinanb@users.noreply.github.com>
2020-06-01 18:46:52 -0500

    :sparkles: El usuarioa ahora puede cambiar su imagen de perfil

--- src/ui/trainer/profile/components/TrainerProfile.js
+++ src/ui/trainer/profile/components/TrainerProfile.js
@@ -7,32 +7,10 @@ import UploadPage from './upload_page/upload_page';
 
 import fire from "../../../../fire/Fire"
 import { gql } from 'apollo-boost';
-import { useQuery } from '@apollo/react-hooks';
+import { useQuery, useMutation } from '@apollo/react-hooks';
 
 function storageData(ref, img) {
   return fire.storage().ref(ref).put(img);
-}
-
-function addImagen(img, id, loadImg, error, fullyLoaded) {
-
-  const task = storageData(`/TrainerPhotos/${id}`, img);
-
-  task.on('state_changed',
-    snapshot => {
-      let percentage = (snapshot.bytesTransferred / snapshot.totalBytes) * 100
-      loadImg(percentage)
-    },
-    error,
-    async () => {
-      try {
-        let url = await task.snapshot.ref.getDownloadURL();
-        //await this.firebaseCreateRepository.writeDocument("usuarios/" + this.getUserId(), { foto: url }, true)
-        fullyLoaded()
-      } catch (errorCatch) {
-        error(errorCatch)
-      }
-    }
-  )
 }
 
 const GET_TOKEN = gql`
@@ -42,6 +20,24 @@ query getToken{
 }
 `
 
+const GET_PROFILE = gql`
+  query GetProfile($token: String!){
+    profileTrainer(token: $token){
+      trainer_name,
+      age,
+      photo,
+      telephone,
+      city,
+      description,
+      specialities,
+      resources,
+      work_experience,
+      sum_ratings,
+      num_ratings
+    }
+  }
+`;
+
 const GET_ID = gql`
 query getID($token: String!){
   authValidateAuthToken(token:$token) {
@@ -50,28 +46,70 @@ query getID($token: String!){
 }
 `
 
+const UPDATE_PROFILE = gql`
+mutation updateProfile($token:String!,$trainer_name:String!,$age:Int!,$photo:String!,$telephone:String!,$city:String!,$sum_ratings:Int!,$num_ratings:Int!,$description:String!,$work_experience:String!,$resources:String!){
+  updateProfileTrainer(token:$token
+  body:{
+    trainer_name:$trainer_name,
+    age:$age,
+    photo:$photo,
+    telephone:$telephone,
+    city:$city,
+    sum_ratings:$sum_ratings,
+    num_ratings:$num_ratings,
+    description:$description,
+    work_experience:$work_experience,
+    resources:$resources,
+  }) {
+    trainer_id
+    trainer_name
+    age
+    photo
+    telephone
+    city
+    sum_ratings
+    num_ratings
+    description
+    work_experience
+    resources
+  }
+}
+`
+
 const TrainerProfile = (props) => {
 
   /*ACTULIZACION DE IMAGEN*/
 
+  const token = useQuery(GET_TOKEN).data.token;
+
+  const { loading: loadingProfile, error: errorProfile, data: dataProfile } = useQuery(GET_PROFILE, {
+    variables: { token, }
+  });
+
   const [uploadImage, setUploadImage] = useState(null);
+
+  const [updateProfile] = useMutation(UPDATE_PROFILE,
+    {
+      update(cache) {
+
+      }
+    })
 
   const [uploadimgstate, setUploadimgstate] = useState(false)
   const [percentageImageLoading, setPercentageImageLoading] = useState(0)
 
-  const token = useQuery(GET_TOKEN).data.token;
 
-  let { data, error, loading } = useQuery(GET_ID,{ variables: { token: token } })
+  let { data, error, loading } = useQuery(GET_ID, { variables: { token: token } })
 
-  if (error) {
+  if (error || errorProfile) {
     return <div>error</div>
   }
 
-  if (loading) {
+  if (loading || loadingProfile) {
     return <div>loading...</div>
   }
 
-  console.log("DATA",data.authValidateAuthToken.ID)
+  console.log("DATA", data.authValidateAuthToken.ID)
 
   const id = data.authValidateAuthToken.ID;
 
@@ -83,9 +121,38 @@ const TrainerProfile = (props) => {
       error => { console.log(error) },
       () => {
         setPercentageImageLoading(100)
-        setUploadimgstate(false)
-        console.log("TERMINOOOOOOO")
+        setUploadImage(null)
+        console.log("TERMINOOOOOOO Terminar")
         //dispatch(fetchUsers())
+      }
+    )
+  }
+
+  const variables = dataProfile.profileTrainer;
+
+  function addImagen(img, id, loadImg, error, fullyLoaded) {
+
+    const task = storageData(`/TrainerPhotos/${id}`, img);
+
+    task.on('state_changed',
+      snapshot => {
+        let percentage = (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+        loadImg(percentage)
+      },
+      error,
+      async () => {
+        try {
+          let url = await task.snapshot.ref.getDownloadURL();
+          variables.photo = url;
+          variables.token = token;
+          //await this.firebaseCreateRepository.writeDocument("usuarios/" + this.getUserId(), { foto: url }, true)
+          console.log("URL", url)
+          console.log("VARIABLES", variables)
+          updateProfile({ variables: variables });
+          fullyLoaded()
+        } catch (errorCatch) {
+          error(errorCatch)
+        }
       }
     )
   }
@@ -114,16 +181,35 @@ const TrainerProfile = (props) => {
     src = "http://lorempixel.com/400/400/sports/";
   }
 
+  console.log("props.trainerData.specialities", props.trainerData)
 
+  let uploadImages;
+
+  if (uploadImage) {
+    uploadImages = <UploadPage
+      percentageImageLoading={percentageImageLoading}
+      onClickCancelar={() => { setUploadImage(null); setPercentageImageLoading(0); }}
+      foto={src}
+      onClickActualizar={(img) => { perfilImghandler(img) }}
+    />
+  }
+
+  console.log("uploadImages",uploadImages);
 
   return (
     <>
-      {uploadImage}
+      {uploadImages}
       <div className="container mt-4">
         <div className="row justify-content-center">
           <div className="col-12 col-md-3 p-0 ">
             <div className="card">
-              <img className="card-img-top" src={src} alt={props.trainerData.trainer_name} onClick={() => setUploadImage(<UploadPage onClickCancelar={() => { setUploadImage(null); setPercentageImageLoading(0) }} onClickActualizar={(img) => { perfilImghandler(img) }} />)} />
+              <img className="card-img-top" src={src} alt={props.trainerData.trainer_name}
+                onClick={
+                  () => {
+                    setPercentageImageLoading(0);
+                    setUploadImage(true)
+                  }
+                } />
               <div className="card-body">
                 <h3 className="card-title FontDarkMainColor mb-4">{props.trainerData.trainer_name}</h3>
                 <h6 className="card-subtitle mb-2 text-muted">Edad:</h6>
