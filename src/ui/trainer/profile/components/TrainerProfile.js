7ab518d2ed3e3121dafba2774ea17b13d0c6ea57
jestupinanb <30636279+jestupinanb@users.noreply.github.com>
2020-07-01 21:36:00 -0500

    :construction:

--- src/ui/trainer/profile/components/TrainerProfile.js
+++ src/ui/trainer/profile/components/TrainerProfile.js
@@ -2,8 +2,156 @@ import React from 'react';
 
 import DegreesList from './DegreesList';
 import SpecialitiesList from './SpecialitiesList';
+import { useState } from 'react';
+import UploadPage from './upload_page/upload_page';
+
+import fire from "../../../../fire/Fire"
+import { gql } from 'apollo-boost';
+import { useQuery, useMutation } from '@apollo/react-hooks';
+
+function storageData(ref, img) {
+  return fire.storage().ref(ref).put(img);
+}
+
+const GET_TOKEN = gql`
+query getToken{
+  token @client
+  type @client
+}
+`
+
+const GET_PROFILE = gql`
+  query GetProfile($token: String!){
+    profileTrainer(token: $token){
+      trainer_name,
+      age,
+      photo,
+      telephone,
+      city,
+      description,
+      specialities,
+      resources,
+      work_experience,
+      sum_ratings,
+      num_ratings
+    }
+  }
+`;
+
+const GET_ID = gql`
+query getID($token: String!){
+  authValidateAuthToken(token:$token) {
+    ID
+  }
+}
+`
+
+const UPDATE_PROFILE = gql`
+mutation updateProfile($token:String!,$trainer_name:String!,$age:Int!,$photo:String!,$telephone:String!,$city:String!,$sum_ratings:Int!,$num_ratings:Int!,$description:String!,$work_experience:String!,$resources:String!){
+  updateProfileTrainer(token:$token
+  body:{
+    trainer_name:$trainer_name,
+    age:$age,
+    photo:$photo,
+    telephone:$telephone,
+    city:$city,
+    sum_ratings:$sum_ratings,
+    num_ratings:$num_ratings,
+    description:$description,
+    work_experience:$work_experience,
+    resources:$resources,
+  }) {
+    trainer_id
+    trainer_name
+    age
+    photo
+    telephone
+    city
+    sum_ratings
+    num_ratings
+    description
+    work_experience
+    resources
+  }
+}
+`
 
 const TrainerProfile = (props) => {
+
+  /*ACTULIZACION DE IMAGEN*/
+
+  const token = useQuery(GET_TOKEN).data.token;
+
+  const { loading: loadingProfile, error: errorProfile, data: dataProfile } = useQuery(GET_PROFILE, {
+    variables: { token, }
+  });
+
+  const [uploadImage, setUploadImage] = useState(null);
+
+  const [updateProfile] = useMutation(UPDATE_PROFILE,
+    {
+      update(cache) {
+
+      }
+    })
+
+  const [percentageImageLoading, setPercentageImageLoading] = useState(0)
+
+
+  let { data, error, loading } = useQuery(GET_ID, { variables: { token: token } })
+
+  if (error || errorProfile) {
+    return <div>error</div>
+  }
+
+  if (loading || loadingProfile) {
+    return <div>loading...</div>
+  }
+
+  const id = data.authValidateAuthToken.ID;
+
+  const perfilImghandler = async (img) => {
+    addImagen(
+      img,
+      id,
+      percentage => { setPercentageImageLoading(percentage) },
+      error => { console.log(error) },
+      () => {
+        setPercentageImageLoading(100)
+        setUploadImage(null)
+      }
+    )
+  }
+
+  const variables = dataProfile.profileTrainer;
+
+  function addImagen(img, id, loadImg, error, fullyLoaded) {
+
+    const task = storageData(`/TrainerPhotos/${id}`, img);
+
+    task.on('state_changed',
+      snapshot => {
+        let percentage = (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+        loadImg(percentage)
+      },
+      error,
+      async () => {
+        try {
+          let url = await task.snapshot.ref.getDownloadURL();
+          variables.photo = url;
+          variables.token = token;
+          console.log("URL", url)
+          console.log("VARIABLES", variables)
+          updateProfile({ variables: variables });
+          fullyLoaded()
+        } catch (errorCatch) {
+          error(errorCatch)
+        }
+      }
+    )
+  }
+
+  /*FIN ACTULIZACION DE IMAGEN*/
 
   if (!props.trainerData) {
     return (
@@ -17,6 +165,7 @@ const TrainerProfile = (props) => {
     );
   }
 
+
   let src;
 
   if (props.trainerData.photo !== 'none') {
@@ -25,48 +174,73 @@ const TrainerProfile = (props) => {
     src = "http://lorempixel.com/400/400/sports/";
   }
 
+  /*ACTULIZACION DE IMAGEN*/
+
+  let uploadImages;
+
+  if (uploadImage) {
+    uploadImages = <UploadPage
+      percentageImageLoading={percentageImageLoading}
+      onClickCancelar={() => { setUploadImage(null); setPercentageImageLoading(0); }}
+      foto={src}
+      onClickActualizar={(img) => { perfilImghandler(img) }}
+    />
+  }
+
+  /*FIN ACTULIZACION DE IMAGEN*/
+
+  console.log("uploadImages",uploadImages);
 
   return (
-    <div className="container mt-4">
-      <div className="row justify-content-center">
-        <div className="col-12 col-md-3 p-0 ">
-          <div className="card">
-            <img className="card-img-top" src={src} alt={props.trainerData.trainer_name} />
-            <div className="card-body">
-              <h3 className="card-title FontDarkMainColor mb-4">{props.trainerData.trainer_name}</h3>
-              <h6 className="card-subtitle mb-2 text-muted">Edad:</h6>
-              <h5 className="card-text">{props.trainerData.age}</h5>
+        <>
+      {uploadImages}
+      <div className="container mt-4">
+        <div className="row justify-content-center">
+          <div className="col-12 col-md-3 p-0 ">
+            <div className="card">
+              <img className="card-img-top" src={src} alt={props.trainerData.trainer_name}
+                onClick={
+                  () => {
+                    setPercentageImageLoading(0);
+                    setUploadImage(true)
+                  }
+                } />
+              <div className="card-body">
+                <h3 className="card-title FontDarkMainColor mb-4">{props.trainerData.trainer_name}</h3>
+                <h6 className="card-subtitle mb-2 text-muted">Edad:</h6>
+                <h5 className="card-text">{props.trainerData.age}</h5>
+              </div>
+            </div>
+            <div className="d-flex justify-content-center my-4">
+              <button className="btn btn-warning btn-lg" onClick={() => { props.goToEditProfile() }}>Editar Perfil</button>
             </div>
           </div>
-          <div className="d-flex justify-content-center my-4">
-            <button className="btn btn-warning btn-lg" onClick={() => { props.goToEditProfile() }}>Editar Perfil</button>
+          <div className="col-12 col-md-5">
+            <div className="WhiteColor border p-3 mb-3">
+              <h3 className="FontDarkMainColor">Contacto</h3>
+              <p>Telefono:</p>
+              <h4>{props.trainerData.telephone}</h4>
+              <p>Ciudad:</p>
+              <h4>{props.trainerData.city}</h4>
+              <h3 className="FontDarkMainColor mt-4">Descripción</h3>
+              <h6>{props.trainerData.description}</h6>
+              <h3 className="FontDarkMainColor mt-4">Experiencia laboral</h3>
+              <h6>{props.trainerData.work_experience}</h6>
+              <h3 className="FontDarkMainColor mt-4">Recursos</h3>
+              <h6>{props.trainerData.resources}</h6>
+            </div>
+          </div>
+          <div className="col-12 col-md-4">
+            <DegreesList
+              degreesData={props.degreesData}
+            />
+            <SpecialitiesList
+              specialities={props.trainerData.specialities}
+            />
           </div>
         </div>
-        <div className="col-12 col-md-5">
-          <div className="WhiteColor border p-3 mb-3">
-            <h3 className="FontDarkMainColor">Contacto</h3>
-            <p>Telefono:</p>
-            <h4>{props.trainerData.telephone}</h4>
-            <p>Ciudad:</p>
-            <h4>{props.trainerData.city}</h4>
-            <h3 className="FontDarkMainColor mt-4">Descripción</h3>
-            <h6>{props.trainerData.description}</h6>
-            <h3 className="FontDarkMainColor mt-4">Experiencia laboral</h3>
-            <h6>{props.trainerData.work_experience}</h6>
-            <h3 className="FontDarkMainColor mt-4">Recursos</h3>
-            <h6>{props.trainerData.resources}</h6>
-          </div>
         </div>
-        <div className="col-12 col-md-4">
-          <DegreesList
-            degreesData={props.degreesData}
-          />
-          <SpecialitiesList
-            specialities={props.trainerData.specialities}
-          />
-        </div>
-      </div>
-    </div>
+        </>
   )
 }
 
